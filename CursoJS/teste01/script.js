56156fd8f496089a7b368d73c43474d0013d4985
Cristian0-Pereira <cristianops2020@gmail.com>
2024-01-12 00:54:17 -0300

    exercicio

--- CursoJS/teste01/script.js
+++ CursoJS/teste01/script.js
@@ -1,16 +1,29 @@
 function carregar() {
-    var msg = document.querySelector('#msg')
-    var img = document.querySelector('#imagem')
+    var mensagem = document.querySelector(".mensagem")
+    var imagem = document.querySelector(".imagem")
 
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 10
+    var hora = data.getHours()
+    // Hora completa abaixo
     hora = hora<10?"0"+hora:hora
     var min = data.getMinutes()
     min = min<10?"0" + min:min
     var seg = data.getSeconds()
     seg = seg<10?"0" + seg:seg
-    const hora_completa = hora+":"+min+":"+seg
-    msg.innerHTML = `Agora são ${hora_completa}h`
     const intervalo = setInterval(carregar,1000)
+    mensagem.innerHTML = `Agora são ${hora}:${min}:${seg}h`
+    
+    if (hora >= 0 && hora < 12) {
+        //Bom dia
+        imagem.src = "img/manha.jpg"
+        document.body.style.background = "#84baf8"
+    }else if (hora >= 12 && hora < 18) {
+        //Boa tarde
+        imagem.src = "img/tarde.jpg"
+        document.body.style.background = "#f1b84e"
+    }else {
+        //Boa noite
+        imagem.src = "img/noite.jpg"
+        document.body.style.background = "#383737"
+    }
 }
