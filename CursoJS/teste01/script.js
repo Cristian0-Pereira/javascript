2f296c9e84c19ffab65e8caf29ef3ae1d76b0ea2
Cristian0-Pereira <cristianops2020@gmail.com>
2024-01-14 02:35:27 -0300

    ad

--- CursoJS/teste01/script.js
+++ CursoJS/teste01/script.js
@@ -5,7 +5,7 @@ function carregar() {
     let saudacao = document.querySelector('.saudacao')
 
     var data = new Date()
-    var hora = 22//data.getHours()
+    var hora = data.getHours()
     // Hora completa abaixo
     hora = hora<10?"0"+hora:hora
     var min = data.getMinutes()
@@ -18,18 +18,18 @@ function carregar() {
         //Bom dia
         imagem.src = "img/manha.jpg"
         document.body.style.background = "#84baf8"
-        saudacao.innerHTML = `Bom dia ${nome}!!!`
+        saudacao.innerHTML = `Bom dia <strong>${nome}</strong>!!!`
     }else if (hora >= 12 && hora < 18) {
         //Boa tarde
         imagem.src = "img/tarde.jpg"
         document.body.style.background = "#f1b84e"
-        saudacao.innerHTML = `Bom tarde ${nome}!!!`
+        saudacao.innerHTML = `Bom tarde <strong>${nome}</strong>!!!`
     }else {
         //Boa noite
         imagem.src = "img/noite.jpg"
         document.body.style.background = "#383737"
         document.querySelector('a').style.color = '#fff'
         document.querySelector('header').style.color = '#fff'
-        saudacao.innerHTML = `Bom noite ${nome}!!!`
+        saudacao.innerHTML = `Bom noite <strong>${nome}</strong>!!!`
     }
 }
