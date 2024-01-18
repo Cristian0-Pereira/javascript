7d71f5f690e2f29980309d28afb29b9f577d224c
Cristian0-Pereira <cristianops2020@gmail.com>
2024-01-18 19:55:58 -0300

    exercicios

--- CursoJS/teste02/script.js
+++ CursoJS/teste02/script.js
@@ -9,29 +9,44 @@ function verificar() {
         let fsex = document.getElementsByName('radsex')
         let idade = ano - Number(fano.value)
         let genero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+            img.style.width = '300px'
+            img.style.height = '250px'
+            img.style.borderRadius = '50%'
+            img.style.marginTop = '10px'
+
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'img/bebe-m.jpg')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'img/jovem-m.jpg')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'img/adulto-m.jpg')
             } else {
                 // Idoso
+                img.setAttribute('src', 'img/idoso-m.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'img/bebe-f.jpg')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'img/jovem-f.jpg')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'img/adulto-f.jpg')
             } else {
                 // Idoso
-            }
-        }
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+                img.setAttribute('src', 'img/idoso-f.jpg')
+            }        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
