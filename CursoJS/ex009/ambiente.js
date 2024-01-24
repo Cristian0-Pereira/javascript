2872a9b5ccf5ce55ec52c64147558122bb719675
Cristian0-Pereira <cristianops2020@gmail.com>
2024-01-24 00:56:24 -0300

    ad

---
+++ CursoJS/ex009/ambiente.js
@@ -0,0 +1,18 @@
+var c = 0
+while (c < 6) {
+    console.log('Tudo bem?')
+    c++
+}
+
+
+
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ CursoJS/ex009/node_ff7ebf4bc55f8.tmp.js
@@ -0,0 +1,17 @@
+var c = 0
+while (c < 6) {
+    console.log('Tudo bem?')
+}
+
+
+
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
